7e85fe26ca3e51be923db289966b9057c4a84bea
ManliestBen <benjamintmanley@gmail.com>
2024-10-23 13:18:03 -0500

    add NaN and random number generation along with rounding example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,6 @@
 package org.example;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +17,32 @@ public class Main {
 //        System.out.println(num4); // works perfectly
 
 //      Math operators
-        System.out.println(Math.pow(2,5)); // squaring a number
-        System.out.println(Math.sqrt(64)); // square root of a number
+//        System.out.println(Math.pow(2,5)); // squaring a number
+//        System.out.println(Math.sqrt(64)); // square root of a number
+
+
+//      NaN
+//        System.out.println(Math.sqrt(-3)); // results in NaN
+//        System.out.println(Double.isNaN(Math.sqrt(-3)));  // check for NaN
+
+//      Random Numbers
+//        System.out.println(Math.random()); // double between 0.0 and 0.99999999 (up to but not including 1)
+
+        int max = 100;
+        int min = 1;
+
+//        System.out.println((Math.random() * (max - min)) + min);
+
+//      One way of checking the type of primitive
+        Object random = (Math.random() * (max - min)) + min;
+//        System.out.println(random.getClass().getName());
+
+//      Getting a random rounded float
+        float random2 = Math.round((Math.random() * (max - min)) + min);
+//        System.out.println(random2);
+
+//      Getting a random int
+        int randomInt = ThreadLocalRandom.current().nextInt(min, max + 1);
+//        System.out.println(randomInt);
     }
 }
