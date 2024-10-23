c03751629be26bcdb0e6195b873ad1577e4a7f06
ManliestBen <benjamintmanley@gmail.com>
2024-10-23 14:54:15 -0500

    add info about strings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -44,5 +45,67 @@ public class Main {
 //      Getting a random int
         int randomInt = ThreadLocalRandom.current().nextInt(min, max + 1);
 //        System.out.println(randomInt);
+
+//      Java variables are pass by value
+//      If the value stored in a variable is a primitive, it's similar to making a copy
+//      at that point in time
+
+        int theseWaffles = 5;
+        int thoseWaffles = theseWaffles;
+
+        theseWaffles += 10;
+
+//        System.out.println("theseWaffles = " + theseWaffles);
+//        System.out.println("thoseWaffles = " + thoseWaffles);
+
+//      When using objects (created by a class), the variables hold references
+//      to the value in memory, rather than just the value itself
+
+        int[] someNums = {1, 2, 3, 4, 5};
+        int[] moreNums = someNums;
+
+        someNums[0] = 4; // This changes the value that both variables point to
+//        someNums = new int[] {3, 4, 5, 6, 9}; // This makes a new array at a new memory address
+//        System.out.println("someNums = " + Arrays.toString(someNums));
+//        System.out.println("moreNums = " + Arrays.toString(moreNums));
+
+//      Strings are immutable, a new value is stored in a new memory address when reassigned to
+
+        String favFood = "waffle";
+        String otherFavFood = favFood;
+
+        favFood = "taco"; // This technically creates a new string at a new memory address
+
+//        System.out.println("favFood = " + favFood);
+//        System.out.println("otherFavFood = " + otherFavFood);  // Still points to the old memory address
+
+//      Multiple ways to define strings
+        String myFirstName = "Ben";
+        String myLastName = new String("Manley");
+        String myMiddleName; // initialized as null
+        myMiddleName = "Thomas";
+
+//      String methods
+
+//      Find the length of a string
+        System.out.println(myFirstName.length());
+//      Find a character at a specific index
+        System.out.println(myFirstName.charAt(2));
+//      Replace part of a string
+        System.out.println(myLastName.replace("M", "St"));
+//      Uppercase
+        System.out.println(myLastName.toUpperCase());
+//      Lowercase
+        System.out.println(myLastName.toLowerCase());
+//      Compare lexicographically (0 means equal, <0 if less than arg, >0 if greater than arg)
+        System.out.println(myLastName.compareTo(myFirstName));
+        System.out.println(myLastName.compareTo(myMiddleName));
+        System.out.println(myLastName.compareTo("Manley"));
+//      Use compareToIgnoreCase() to ignore case
+        System.out.println(myLastName.compareTo("manley"));
+//      Check if string contains a set of characters
+        System.out.println(myLastName.contains("Man"));
+//      Checking equality
+        System.out.println(myMiddleName.equals("Banana"));
     }
 }
