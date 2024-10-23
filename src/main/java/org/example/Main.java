d69987f4a04a62f87cbe25e218b7ffe39194bfa3
ManliestBen <benjamintmanley@gmail.com>
2024-10-23 12:33:49 -0500

    add declarations and math operators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Setup is complete");
+//      Declarations
+        int num1 = 4;
+        int num2 = 6 / 4;
+//        System.out.println(num1);
+//        System.out.println(num2);  // shows up as 1 because of int
+
+        float num3 = 6 / 4;
+//        System.out.println(num3);  // still doesn't work, 6 and 4 are int types
+
+        float num4 = 6f / 4f;
+//        System.out.println(num4); // works perfectly
+
+//      Math operators
+        System.out.println(Math.pow(2,5)); // squaring a number
+        System.out.println(Math.sqrt(64)); // square root of a number
     }
 }
